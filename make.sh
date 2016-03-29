81bef6b7d2c00f43cc9fc3f7b23c31d579879cca
Ben Black <weepingwillowben@reed.edu>
2016-03-28 20:30:48 -0700

    build happened

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-g++ -std=c++11 -O2 -w -fpermissive -D ASIO_STANDALONE -I "impl/" -I "asio/include/" -o server server.cpp impl/hash_cache.c impl/helper.c impl/lru_replacement.c
+g++ -std=c++11 -O2 -pthread -w -fpermissive -D ASIO_STANDALONE -I "impl/" -I "asio/include/" -o server server.cpp impl/hash_cache.c impl/helper.c impl/lru_replacement.c
 #g++ -std=c++11 -O2 -I "test/" -I "asio/include/" -o client_tests client.cpp test/basic_test.c test/lrutests.c test/main.c test/test.c test/test_helper.c
-./server
+./server -p 9300 -m 100000
 #./client_tests
